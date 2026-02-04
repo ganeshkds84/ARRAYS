e9e3c83e2cb16afbb354e5f278f3ad46c2bae704
GANESH <ganeshk.ds84@gmail.com>
2026-02-04 18:57:16 +0530

    modified

--- 012_problem.py
+++ 012_problem.py
@@ -1,24 +1,25 @@
-def solve(nums):
-    l = 0
-    i = 0
-    r = len(nums) - 1
+class Solution:
+    def sortZeroOneTwo(self, nums):
+        l = 0
+        i = 0
+        r = len(nums) - 1
 
-    while i <= r:
-        if nums[i] == 0:
-            nums[i], nums[l] = nums[l], nums[i]
-            l += 1
-            i += 1
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
 
-        elif nums[i] == 2:
-            nums[i], nums[r] = nums[r], nums[i]
-            r -= 1
-            # i is NOT incremented here
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+                # i is NOT incremented here
 
-        else:  # nums[i] == 1
-            i += 1
+            else:  # nums[i] == 1
+                i += 1
 
-    return nums
-
+        return nums
 
 a = list(map(int, input("Enter only 0,1 and 2's: ").split()))
-print(solve(a))
+obj = Solution()
+print(obj.sortZeroOneTwo(a))

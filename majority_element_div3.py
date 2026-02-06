aa264c3098cc5d707edf016c8e4aa815062d4642
GANESH <ganeshk.ds84@gmail.com>
2026-02-06 19:26:51 +0530

    Optimized version

--- majority_element_div3.py
+++ majority_element_div3.py
@@ -1,18 +1,37 @@
 class Solution():
     def majority_element(self,nums):
-        seen={}
-        final=[]
+        m1=None
+        m2=None
+        c1=0
+        c2=0
         for num in nums:
-            if num not in seen:
-                seen[num]=1
+            if c1==0:
+                m1=num
+                c1=1
+            elif c2==0 and m1!=num:
+                m2=num
+                c2=1
+            elif m1==num:
+                c1+=1
+            elif m2==num:
+                c2+=1
             else:
-                seen[num]=seen[num]+1
-        for i in seen:
-            if seen[i]>len(nums)//3:
-                final.append(i)
-        
-        return final
+                c1-=1
+                c2-=1
+        c1,c2=0,0
+        for num in nums:
+            if num==m1:
+                c1+=1
+            elif num==m2:
+                c2+=1
+        result=[]
+        if c1>len(nums)//3:
+            result.append(m1)
+        if c2>len(nums)//3:
+            result.append(m2)
+        return result
     
+
 a=int(input("Enter list size:"))        
 b=list(map(int,input("Enter the numbers:").split()))
 ans=Solution()

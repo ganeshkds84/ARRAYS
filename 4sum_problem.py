8cedeea6ab9b92bf92e24e3fb2764ea468fe71c0
GANESH <ganeshk.ds84@gmail.com>
2026-02-10 23:30:41 +0530

    Finding quadraplet whose sum equals to zero

---
+++ 4sum_problem.py
@@ -0,0 +1,42 @@
+class Gowtham():
+    def four_sum(self,nums):
+        final=[]
+        nums=sorted(nums)
+        for i in range(len(nums)-3):
+            t1=nums[i]
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            j=i+1
+            while j<len(nums)-2:
+                t2=nums[j]
+                if j>i+1 and nums[j]==nums[j-1]:
+                    j+=1
+                    continue
+                k=j+1
+                l=len(nums)-1
+                while k<l:
+                    t3=nums[k]
+                    t4=nums[l]
+                    req=t1+t2+t3+t4
+                    
+                    if req==0:
+                        final.append([t1,t2,t3,t4])
+                        k+=1
+                        l-=1
+                        while k<l and nums[k]==nums[k-1]:
+                            k+=1
+                        while k<l and nums[l]==nums[l+1]:
+                            l-=1
+                    elif req>0:
+                        l-=1
+                    else:
+                        k+=1
+                j+=1   
+        return final
+                
+        
+ansr=Gowtham()
+print(ansr.four_sum([1,0,-1,0,-2,2]))
+
+
+    

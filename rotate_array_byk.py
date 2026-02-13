5d35bf7f07fb9ab98031ab7e59fbf23302b50bc9
GANESH <ganeshk.ds84@gmail.com>
2026-02-13 18:10:57 +0530

    Optimized the space complexity

--- rotate_array_byk.py
+++ rotate_array_byk.py
@@ -1,13 +1,25 @@
 class Solution():
     def rotate_byk(self,nums,k):
-        nums=nums+nums[:k+1]
-        print(nums)
-        nums=nums[k:]
-        
-        
-        return nums
-    
-    
+        k=k%len(nums)
+        f=0
+        l=k-1
+        while f<l:
+            nums[f],nums[l]=nums[l],nums[f]
+            f+=1
+            l-=1
+        f=k
+        l=len(nums)-1
+        while f<l:
+            nums[f],nums[l]=nums[l],nums[f]
+            f+=1
+            l-=1
+        f=0
+        l=len(nums)-1
+        while f<l:
+            nums[f],nums[l]=nums[l],nums[f]
+            f+=1
+            l-=1
+        return nums    
     
 answer=Solution()
-print(answer.rotate_byk([1,2,3,4,5],2))
+print(answer.rotate_byk([1,2,3,4,5,6,7,8],3))

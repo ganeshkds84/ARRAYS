c0c196ca21b41b8d55ec33c4f8f169b814081c2a
GANESH <ganeshk.ds84@gmail.com>
2026-02-28 12:30:53 +0530

    Optimized version

--- longest_subarray.py
+++ longest_subarray.py
@@ -1,13 +1,16 @@
 def longest_subarray(nums,k):
     length,final=0,0
+    seen={}
+    c_sum=0
     for i in range(len(nums)):
-        c_sum=0
-        #c_sum=nums[i]
-        for j in range(i,len(nums)):
-            c_sum=c_sum+nums[j]
-            if c_sum==k:
-                length=j-i+1
-                final=max(length,final)
+        c_sum=c_sum+nums[i]
+        if c_sum==k:
+            final=i+1
+        if (c_sum-k) in seen:
+            length=i-seen[c_sum-k]
+            final=max(length,final)
+        if (c_sum-k) not in seen:
+            seen[c_sum]=i
     
     return final            
     

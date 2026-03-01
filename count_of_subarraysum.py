c051e76b33155280b7519cc62fee6696e4779e75
GANESH <ganeshk.ds84@gmail.com>
2026-03-01 12:20:05 +0530

    Optimized version

--- count_of_subarraysum.py
+++ count_of_subarraysum.py
@@ -1,17 +1,16 @@
 class Ganesh():
     def count_of_subarrays(self,nums,k):
         count=0
+        seen={0:1}
+        c_sum=0
         for i in range(len(nums)):
-            c_sum=0
-            for j in range(i,len(nums)):
-                c_sum=c_sum+nums[j]
-                
-                if c_sum==k:
-                    count+=1
-                    
+            c_sum=c_sum+nums[i]
+            if (c_sum-k) in seen:
+                count+=seen[c_sum-k]
+            seen[c_sum]=seen.get(c_sum,0)+1
         return count
     
 Ashwika=Ganesh()
-nums=[1,1,1,1,1]
-k=3
+nums = [1, 2, 1, 2, 1]
+k = 3
 print(Ashwika.count_of_subarrays(nums,k))
